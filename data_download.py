f8102e18da77a20095c270e2aac7ea0cc71d5cd9
blade-soul <CJHdream@outlook.com>
2020-10-28 15:14:56 +0800

    update

--- data_download.py
+++ data_download.py
@@ -1,9 +1,6 @@
 #In[]:
-<<<<<<< HEAD
-=======
 # coding=utf-8
 import collections
->>>>>>> 4056e69140962c872d961de69466fd514282acb4
 import pandas as pd
 from requests_html import HTMLSession
 
@@ -27,38 +24,6 @@ zong_session = session.get(url)
 #In[]:
 sel = 'body > div#main > div#main_left > div.kuang > div#main_content > div.xiaoshuomingkuang_neirong > p'
 #r.html.find('body')[0].find('div')
-<<<<<<< HEAD
-leibies = zong_session.html.find(sel)[1]
-
-leibie_dfs = dict()
-
-#for leibie in leibies:
-leibie_name, leibie_sub_url = leibie.text, list(leibie.links)[0]
-leibie_url = '{}{}'.format(url, v)
-leibie_session = session.get(leibie_url)
-shiwu_sel = "body > div#main > div#main_left > div.kuang > div#main_content > ul.list > li"
-shiwues = leibie_session.html.find(shiwu_sel)
-shiwu_df = pd.DataFrame()
-#for shiwu in shiwues:
-shiwu = shiwues[0]
-shiwu_dict = dict()
-shiwu_name = shiwu.text
-shiwu_dict['名称'] = shiwu_name
-shiwu_sub_url = list(shiwu.links)[0]
-shiwu_yinyangchengfen_url = '{}{}'.format(url, shiwu_sub_url)
-shiwu_dict['url'] = shiwu_yinyangchengfen_url
-print(shiwu_yinyangchengfen_url)
-shiwu_yinyangchengfen_session = session.get(shiwu_yinyangchengfen_url)
-shiwu_yinyang_sel = "body > div#main > div#main_left > div.kuang > div#main_content > table > tr"
-yinyangchengfens = shiwu_yinyangchengfen_session.html.find(shiwu_yinyang_sel)
-#for yinyangchenfen in yinyangchengfens:
-yinyangchengfen = yinyangchengfens[0]
-yinyangchengfen = yinyangchengfen.text.split('\n')
-shiwu_dict[yinyangchengfen[0]] = yinyangchengfen[1]
-shiwu_dict[yinyangchengfen[2]] = yinyangchengfen[3]
-shiwu_df = shiwu_df.append(shiwu_dict)
-shiwu_df.set_index(['名称']， inplace)
-=======
 leibies = zong_session.html.find(sel)[1].find("a")
 
 leibie_dfs = collections.OrderedDict() 
@@ -102,7 +67,6 @@ for leibie in leibies:
     writer.save()
     print(leibie_name)
     #break
->>>>>>> 4056e69140962c872d961de69466fd514282acb4
 #In[]
 temp = shiwu_df.append(shiwu_dict, ignore_index=True)
 temp.set_index(['名称'], inplace=True)
